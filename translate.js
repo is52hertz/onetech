841dfa36fe726dce1c93a8d30675a1f8f31182e5
ShameimaruAya <wzszwj2014@gmail.com>
2019-12-22 11:04:54 +0800

    Readd translate.js

---
+++ translate.js
@@ -0,0 +1,84 @@
+var fs = require('fs');
+
+function deleteCache(){
+	console.log('Deleting Cache!');
+	if (fs.existsSync('./objects/cache.fcz')) {
+		fs.unlinkSync('./objects/cache.fcz', function(error) {
+			if (error) {
+				throw error;
+			}
+		});	
+	}
+	
+}
+
+function replaceNonASCII(input){
+	return input;
+	for (i=0;i<input.length;i++)
+	{
+		input[i]=input[i].replace(/[őóö]/ig,"o")
+		input[i]=input[i].replace(/[úűü]/ig,"u")
+		input[i]=input[i].replace(/á/ig,"a")
+		input[i]=input[i].replace(/é/ig,"e")
+		input[i]=input[i].replace(/[ŐÓÖ]/ig,"O")
+		input[i]=input[i].replace(/[ÚÜŰ]/ig,"U")
+		input[i]=input[i].replace(/Á/ig,"A")
+		input[i]=input[i].replace(/É/ig,"E")
+	}
+	return input;
+}
+
+function copyMenu(filename){
+}
+
+function loadfile(filename){
+	deleteCache();
+	copyMenu(filename);
+	if (fs.existsSync(filename)) {
+		fs.readFile(filename, 'utf8', function(err, contents) {		
+			if(contents == undefined)
+				return;
+			contents = contents.split("\n");
+			contents.forEach(function(element) {
+				element = replaceNonASCII(element.replace(/\t/g,''));
+				console.log('Replacing:',element.split(";")[0].replace(/\s/g, '')+'!');
+				replaceTextInFile('./objects/'+element.split(";")[0].replace(/\s/g, ''),element.split(";")[1]);
+			});
+		
+	});
+	}else{
+		console.log("Missing file!")
+	}
+	
+
+}
+
+function replaceTextInFile(filename,newstring){
+		
+	fs.readFile(filename, 'utf8', function(err, contents) {
+			if(contents == undefined)
+				return;
+			contents = contents.split("\n");
+			contents[1] = newstring;// + "\r";
+			console.log(contents[1]);
+			contents = contents.join("\n");
+			fs.writeFile(filename, contents, 'utf8', function(err) {
+			if(err) {
+				return console.log(err);
+			}
+			
+		}); 
+	});
+
+}
+
+//
+if(typeof(process.argv[2])=="undefined"){
+	console.log("Missing argument!\nUse -h for help!");
+}else{
+	if(process.argv[2] == "-h"|| process.argv[2] == "- h"){
+		console.log("The parameter must be an input file. Ex. 'translate.exe hungarian.txt'");
+	}else{
+		loadfile(process.argv[2]);
+	}
+}
